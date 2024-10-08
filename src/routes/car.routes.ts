ac3521181f93fc80da30e2c6112363f8ba3afa0e
Luan Henique Campos Soares <lhcampossoares@gmail.com>
2024-10-08 12:10:21 -0300

    fix: auth nas rotas de car

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -2,15 +2,21 @@ import createCarValidator from '@/api/midlewares/createCarValidator';
 import CarController from '../api/controllers/carController';
 import { Router } from 'express';
 import updateCarValidator from '@/api/midlewares/updateCarValidator';
+import isAuthenticated from '@/api/midlewares/isAuthenticated';
 
 const carRouter = Router();
 const carController = new CarController();
 
-carRouter.post('/', createCarValidator, carController.create);
-carRouter.get('/', carController.list);
-carRouter.get('/:id', carController.show);
-carRouter.put('/:id', updateCarValidator, carController.update);
-carRouter.delete('/:id', carController.delete);
-carRouter.patch('/:id', carController.updateAcessory);
+carRouter.post('/', isAuthenticated, createCarValidator, carController.create);
+carRouter.get('/', isAuthenticated, carController.list);
+carRouter.get('/:id', isAuthenticated, carController.show);
+carRouter.put(
+  '/:id',
+  isAuthenticated,
+  updateCarValidator,
+  carController.update,
+);
+carRouter.delete('/:id', isAuthenticated, carController.delete);
+carRouter.patch('/:id', isAuthenticated, carController.updateAcessory);
 
 export default carRouter;
