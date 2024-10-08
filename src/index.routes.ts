6946b9bb49d90d175a12520b7b059ebe0443ca2e
Luan Henique Campos Soares <lhcampossoares@gmail.com>
2024-10-08 16:08:23 -0300

    docs: documentaçao swagger e rota

--- src/index.routes.ts
+++ src/index.routes.ts
@@ -3,9 +3,12 @@ import carRouter from './routes/car.routes';
 import userRouter from './routes/user.routes';
 import authRouter from './routes/auth.routes';
 import reserveRouter from './routes/reserve.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from 'swagger.json';
 
 const routes = Router();
 
+routes.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 routes.use('/v1/car', carRouter);
 routes.use('/v1/user', userRouter);
 routes.use('/v1/auth', authRouter);
